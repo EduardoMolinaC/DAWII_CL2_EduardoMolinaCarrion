b7bd0ba7adbb95824cbb03f9343699e9be8659c9
EduardoMolinaC <clausmolina1999@gmail.com>
2023-10-28 19:58:15 -0500

    primer commit

---
+++ src/main/java/cibertec/edu/pe/DAWII_CL2_EduardoMolinaCarrion/DawiiCl2EduardoMolinaCarrionApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWII_CL2_EduardoMolinaCarrion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2EduardoMolinaCarrionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2EduardoMolinaCarrionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DAWII_CL2_EduardoMolinaCarrion/DawiiCl2EduardoMolinaCarrionApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWII_CL2_EduardoMolinaCarrion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2EduardoMolinaCarrionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
